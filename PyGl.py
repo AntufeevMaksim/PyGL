f5733437948692cd5ae7f3820f6f40357f96c72e
AntufeevMaksim <antufeevmaksim9@gmail.com>
2023-04-28 20:40:35 +0700

    Library "ImageOPS" changed to "numpy"

--- PyGl.py
+++ PyGl.py
@@ -5,6 +5,8 @@ from obj_parser import ObjParser
 import random
 import math
 from geometry import Vec3
+import numpy as np
+
 
 def v_in_range(list_v, range_x, range_y):
   for v in list_v:
@@ -12,7 +14,7 @@ def v_in_range(list_v, range_x, range_y):
       return True
 
 
-def line(x0, y0, x1, y1, im, color):
+def line(x0, y0, x1, y1, image, color):
 
   steep = False
   if(abs(x1 - x0) < abs(y1 - y0)):
@@ -42,9 +44,9 @@ def line(x0, y0, x1, y1, im, color):
       y += dir_x
       error -= dx
     if (steep):
-      im.putpixel((int(y), x), color)
+      image[y][x] = color
     else:
-      im.putpixel((x, int(y)), color)
+      image[y][x] = color
     error += d_error
     x += 1
 
@@ -57,8 +59,6 @@ def cos_2vec(a, b):
   return (a.x*b.x + a.y*b.y + a.z*b.z)/(math.sqrt((a.x)**2 + (a.y)**2 + (a.z)**2) * math.sqrt((b.x)**2 + (b.y)**2 + (b.z)**2))
 
 def draw_obj(obj, image):
-  count = 0
-  draw = ImageDraw.Draw(image)
   for face in obj.faces:
     v1 = obj.vertexes[face[0]-1]
     v2 = obj.vertexes[face[1]-1]
@@ -79,43 +79,34 @@ def draw_obj(obj, image):
     screen_v3.x = int((v3.x+1) * width)-1
     screen_v3.y = int((v3.y+1) * height)-1
 
-    # if v_in_range((screen_v1, screen_v2, screen_v3), list(range(480, 500)), list(range(850, 880))):
-    #   print(1)
 
     vec1 = [v1.x - v2.x, v1.y - v2.y, v1.z - v2.z]
     vec2 = [v1.x - v3.x, v1.y - v3.y, v1.z - v3.z]
 
-    image.putpixel((488, 863), (255, 0, 0))
 
     normal = vec_product(vec2, vec1)
 
     light_dir = Vec3(0, 0, -1)
     intensity = cos_2vec(normal, light_dir)
 
-    if v_in_range((screen_v1, screen_v2, screen_v3), list(range(498, 501)), list(range(110, 171))):
-      triangle(screen_v1, screen_v2, screen_v3, image, (int(255), int(0), int(0)))
-      ImageOps.flip(image).save("output.tga")
+    if intensity > 0:
+      triangle(screen_v1, screen_v2, screen_v3, image, (int(255*intensity), int(255*intensity), int(255*intensity)))
 
-# #    if (v1.z < 0 or v2.z < 0 or v3.z > 0):
-    triangle(screen_v1, screen_v2, screen_v3, image, (int(random.randrange(0, 256)), int(random.randrange(0, 256)), int(random.randrange(0, 256))))
-#    ImageOps.flip(image).save("output.tga")
-#    draw.polygon([(screen_v1.x, screen_v1.y), (screen_v2.x, screen_v2.y), (screen_v3.x, screen_v3.y)], (int(random.randrange(0, 256)), int(random.randrange(0, 256)), int(random.randrange(0, 256))))
-    count += 1
-
-
-  count += 0
-#    line(copy_v1[0], copy_v1[1], copy_v2[0], copy_v2[1], image, (255, 255, 255))
-#    line(copy_v1[0], copy_v1[1], copy_v3[0], copy_v3[1], image, (255, 255, 255))
-#    line(copy_v2[0], copy_v2[1], copy_v3[0], copy_v3[1], image, (255, 255, 255))  
-
-def change_x(x, y, err, dy, dir, color):
+def change_x(x, y, err, dy, dir, image, color):
   while 2*err > dy:
     x += dir
     err -= dy
-    image.putpixel((x, y), color)
+    image[y][x] = color
 
   return x, err
 
+
+def horizontal_line(x1, x2, y, image, color):
+  if (x1 > x2):
+    x1, x2 = x2, x1
+
+  for x in range(x1, x2+1):
+    image[y][x] = color
 
 
 def triangle(v1, v2, v3, image, color):
@@ -162,61 +153,40 @@ def triangle(v1, v2, v3, image, color):
   err_alpha = 0
   err_beta = 0
 
+  if y1 == y2:
+    horizontal_line(x1, x2, y1, image, color)
+    x_beta = x2
 
-  line(x_alpha, y1, x_beta, y1, image, color)
+
   for y in range(y1+1, y2+1):
     err_alpha += d_err_alpha
-
-    x_alpha, err_alpha = change_x(x_alpha, y, err_alpha, dy1_3, dir_alpha, color)
-    # while 2*err_alpha > dy1_3:
-    #   x_alpha += dir_alpha
-    #   err_alpha -= dy1_3
-    #   image.putpixel((x_alpha, y), color)
+    x_alpha, err_alpha = change_x(x_alpha, y, err_alpha, dy1_3, dir_alpha, image, color)
 
     err_beta += d_err_beta
-    x_beta, err_beta = change_x(x_beta, y, err_beta, dy1_2, dir_beta, color)
-    # while 2*err_beta >= dy1_2:
-    #   x_beta += dir_beta
-    #   err_beta -= dy1_2
-    #   image.putpixel((x_beta, y), color)
+    x_beta, err_beta = change_x(x_beta, y, err_beta, dy1_2, dir_beta, image, color)
 
-    line(x_alpha, y, x_beta, y, image, color)
+    horizontal_line(x_alpha, x_beta, y, image, color)
 
   if x3 > x2:
     dir_beta = 1
   else:
     dir_beta = -1
   d_err_beta = dx2_3
-  line(x_alpha, y2+1, x_beta, y2+1, image, color)
-  for y in range(y2+2, y3+1):
+
+  if y2 == y3:
+    horizontal_line(x2, x3, y2, image, color)
+
+  for y in range(y2+1, y3+1):
 
     err_alpha += d_err_alpha
-    x_alpha, err_alpha = change_x(x_alpha, y, err_alpha, dy1_3, dir_alpha, color)
-    # while 2*err_alpha > dy1_3:
-    #   x_alpha += dir_alpha
-    #   err_alpha -= dy1_3
-    #   image.putpixel((x_alpha, y), color)
+    x_alpha, err_alpha = change_x(x_alpha, y, err_alpha, dy1_3, dir_alpha, image, color)
 
     err_beta += d_err_beta
-    x_beta, err_beta = change_x(x_beta, y, err_beta, dy2_3, dir_beta, color)
-    # while 2*err_beta > dy2_3:
-    #   x_beta += dir_beta
-    #   err_beta -= dy2_3
-    #   image.putpixel((x_beta, y), color)
+    x_beta, err_beta = change_x(x_beta, y, err_beta, dy2_3, dir_beta, image, color)
 
-    line(x_alpha, y, x_beta, y, image, color)
-#  ImageOps.flip(image).save("output.tga")
+    horizontal_line(x_alpha, x_beta, y, image, color)
 
 
-
-
-  # array_x1 = line_triangle(x1, y1, x3, y3, image, color)
-
-  # array_x2 = line_triangle(x1, y1, x2, y2, image, color)
-  # array_x2 += line_triangle(x2, y2, x3, y3, image, color)
-
-  # for i in range(len(array_x1)):
-  #   line(array_x1[i], y1+i, array_x2[i], y1+i, image, color)
 
 
 
@@ -224,20 +194,15 @@ IMAGE_HEIGHT = 1000
 IMAGE_WIDTH = 1000
 
 
-image = Image.new("RGB", (IMAGE_WIDTH, IMAGE_HEIGHT))
+#image = Image.new("RGB", (IMAGE_WIDTH, IMAGE_HEIGHT))
+
+image_array = np.zeros( (IMAGE_WIDTH,IMAGE_HEIGHT,3), dtype=np.uint8 )
 
 obj = ObjParser("african_head.obj")
 
-draw_obj(obj, image)
+draw_obj(obj, image_array)
 
+image = Image.fromarray(image_array)
 
-
-#triangle((1, 1), (12,6), (6,18), image, (0,255,0))
-
-#triangle((20, 20), (240,120), (120,360),   image, (0,255,0))
-
-#line(1,1, 7,3, image, (255,255,255))
 
 ImageOps.flip(image).save("output.tga")
-
-#triangle((20, 20), (240,120), (120,360),   image, (0,255,0))
