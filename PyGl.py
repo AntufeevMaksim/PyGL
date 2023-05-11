e2804f65bca61ed98e807324fce9a225eb14b4e5
AntufeevMaksim <antufeevmaksim9@gmail.com>
2023-05-11 20:06:11 +0700

    Refactored triangle function. Added parser for vt.

--- PyGl.py
+++ PyGl.py
@@ -6,7 +6,7 @@ import random
 import math
 import geometry as geo
 import numpy as np
-
+import cv2
 
 def line(x0, y0, x1, y1, image, color):
 
@@ -44,16 +44,25 @@ def line(x0, y0, x1, y1, image, color):
     error += d_error
     x += 1
 
-def draw_obj(obj, image):
+def draw_obj(obj, texture, image):
   z_buffer = np.full((IMAGE_HEIGHT, IMAGE_WIDTH), -np.inf)
   for face in obj.faces:
-    v1 = obj.vertexes[face[0]-1]
-    v2 = obj.vertexes[face[1]-1]
-    v3 = obj.vertexes[face[2]-1]
+    v1 = obj.vertexes[face[0].v-1]  #subtract 1 because in the file the vertex numbering starts from 1 and in the list from 0
+    v2 = obj.vertexes[face[1].v-1]
+    v3 = obj.vertexes[face[2].v-1]
+
+    vt1 = obj.vertexes_textures[face[0].vt-1]
+    vt2 = obj.vertexes_textures[face[1].vt-1]
+    vt3 = obj.vertexes_textures[face[2].vt-1]
+
 
     screen_v1 = geo.Vec3()
     screen_v2 = geo.Vec3()
     screen_v3 = geo.Vec3()
+
+    screen_vt1 = geo.Vec3()
+    screen_vt2 = geo.Vec3()
+    screen_vt3 = geo.Vec3()
 
     width = IMAGE_WIDTH/2
     height = IMAGE_HEIGHT/2
@@ -70,6 +79,19 @@ def draw_obj(obj, image):
     screen_v3.z = v3.z
 
 
+    texture_width = len(texture[0])/2
+    texture_height = len(texture)/2
+    screen_vt1.x = int((vt1.x+1) * texture_width)-1
+    screen_vt1.y = int((vt1.y+1) * texture_height)-1
+
+
+    screen_vt2.x = int((vt2.x+1) * texture_width)-1
+    screen_vt2.y = int((vt2.y+1) * texture_height)-1
+
+
+    screen_vt3.x = int((vt3.x+1) * texture_width)-1
+    screen_vt3.y = int((vt3.y+1) * texture_height)-1
+
     vec1 = [v1.x - v2.x, v1.y - v2.y, v1.z - v2.z]
     vec2 = [v1.x - v3.x, v1.y - v3.y, v1.z - v3.z]
 
@@ -80,7 +102,7 @@ def draw_obj(obj, image):
     intensity = geo.cos_2vec(normal, light_dir)
 
     if intensity > 0:
-      triangle(screen_v1, screen_v2, screen_v3, image, z_buffer, (int(255*intensity), int(255*intensity), int(255*intensity)))
+      triangle(screen_v1, screen_v2, screen_v3, screen_vt1, screen_vt2, screen_vt3, image, z_buffer, (int(255*intensity), int(255*intensity), int(255*intensity)), texture)
 
 def change_x(x, y, err, dy, dir, image, color):
   while 2*err > dy:
@@ -100,37 +122,31 @@ def horizontal_line(x1, x2, y, z1, z2, image, z_buffer, color):
       z_buffer[y][x] = z
       image[y][x] = color
 
-def triangle(v1, v2, v3, image, z_buffer, color):
-  x1, y1 = v1.x, v1.y
-  x2, y2 = v2.x, v2.y
-  x3, y3 = v3.x, v3.y
+def triangle(v1, v2, v3, screen_vt1, screen_vt2, screen_vt3, image, z_buffer, color, texture):
 
-  if (y3 < y1):
-    y1, y3 = y3, y1
-    x1, x3 = x3, x1
+  if (v3.y < v1.y):
+    v1, v3 = v3, v1
 
-  if (y2 < y1):
-    y2, y1 = y1, y2
-    x2, x1 = x1, x2
+  if (v2.y < v1.y):
+    v2, v1 = v1, v2
 
-  if (y3 < y2):
-    y2, y3 = y3, y2
-    x2, x3 = x3, x2
+  if (v3.y < v2.y):
+    v2, v3 = v3, v2
 
-  dy1_3 = y3 - y1 
-  dy1_2 = y2 - y1
-  dy2_3 = y3 - y2
+  dy1_3 = v3.y - v1.y 
+  dy1_2 = v2.y - v1.y
+  dy2_3 = v3.y - v2.y
 
-  dx1_3 = abs(x3 - x1)
-  dx1_2 = abs(x2 - x1)
-  dx2_3 = abs(x3 - x2)
+  dx1_3 = abs(v3.x - v1.x)
+  dx1_2 = abs(v2.x - v1.x)
+  dx2_3 = abs(v3.x - v2.x)
 
-  if x3 > x1:
+  if v3.x > v1.x:
     dir_alpha = 1
   else:
     dir_alpha = -1
 
-  if x2 > x1:
+  if v2.x > v1.x:
     dir_beta = 1
   else:
     dir_beta = -1
@@ -138,39 +154,39 @@ def triangle(v1, v2, v3, image, z_buffer, color):
   d_err_alpha = dx1_3
   d_err_beta = dx1_2
 
-  x_alpha = x1
-  x_beta = x1
+  x_alpha = v1.x
+  x_beta = v1.x
 
   err_alpha = 0
   err_beta = 0
 
-  if y1 == y2:
-    horizontal_line(x1, x2, y1, v1.z, v2.z, image, z_buffer, color)
-    x_beta = x2
+  if v1.y == v2.y:
+    horizontal_line(v1.x, v2.x, v1.y, v1.z, v2.z, image, z_buffer, color)
+    x_beta = v2.x
 
 
-  for y in range(y1+1, y2+1):
+  for y in range(v1.y+1, v2.y+1):
     err_alpha += d_err_alpha
     x_alpha, err_alpha = change_x(x_alpha, y, err_alpha, dy1_3, dir_alpha, image, color)
 
     err_beta += d_err_beta
     x_beta, err_beta = change_x(x_beta, y, err_beta, dy1_2, dir_beta, image, color)
 
-    z_alpha = v1.z + ((v3.z - v1.z)*(y - y1))/(y3 - y1)
-    z_beta = v1.z + ((v2.z - v1.z)*(y - y1))/(y2 - y1)
+    z_alpha = v1.z + ((v3.z - v1.z)*(y - v1.y))/(v3.y - v1.y)
+    z_beta = v1.z + ((v2.z - v1.z)*(y - v1.y))/(v2.y - v1.y)
 
     horizontal_line(x_alpha, x_beta, y, z_alpha, z_beta, image, z_buffer, color)
 
-  if x3 > x2:
+  if v3.x > v2.x:
     dir_beta = 1
   else:
     dir_beta = -1
   d_err_beta = dx2_3
 
-  if y2 == y3:
-    horizontal_line(x2, x3, y2, v2.z, v3.z, image, z_buffer, color)
+  if v2.y == v3.y:
+    horizontal_line(v2.x, v3.x, v2.y, v2.z, v3.z, image, z_buffer, color)
 
-  for y in range(y2+1, y3+1):
+  for y in range(v2.y+1, v3.y+1):
 
     err_alpha += d_err_alpha
     x_alpha, err_alpha = change_x(x_alpha, y, err_alpha, dy1_3, dir_alpha, image, color)
@@ -178,8 +194,8 @@ def triangle(v1, v2, v3, image, z_buffer, color):
     err_beta += d_err_beta
     x_beta, err_beta = change_x(x_beta, y, err_beta, dy2_3, dir_beta, image, color)
 
-    z_alpha = v1.z + ((v3.z - v1.z)*(y - y1))/(y3 - y1)
-    z_beta = v2.z + ((v3.z - v2.z)*(y - y2))/(y3 - y2)
+    z_alpha = v1.z + ((v3.z - v1.z)*(y - v1.y))/(v3.y - v1.y)
+    z_beta = v2.z + ((v3.z - v2.z)*(y - v2.y))/(v3.y - v2.y)
 
     horizontal_line(x_alpha, x_beta, y, z_alpha, z_beta, image, z_buffer, color)
 
@@ -192,12 +208,17 @@ WHITE = (255, 255, 255)
 
 IMAGE_HEIGHT = 1000
 IMAGE_WIDTH = 1000
+
 image = np.zeros( (IMAGE_WIDTH,IMAGE_HEIGHT,3), dtype=np.uint8 )
+#texture = cv2.imread("african_head_diffuse.tga")
 obj = ObjParser("african_head.obj")
 
+texture = Image.open("african_head_diffuse.tga")
+texture.load()
+texture = np.asarray(texture, dtype="int32")
 
-#render(image)
-draw_obj(obj, image)
+draw_obj(obj, texture, image)
+
 
 
 
--- obj_parser.py
+++ obj_parser.py
@@ -1,15 +1,28 @@
 
 from geometry import Vec3
 
+class Face:
+  v = int()
+  vt = int()
+  vn = int()
+
+  def __init__(self, v, vt, vn=0) -> None:
+    self.v = v
+    self.vt = vt
+    self.vn = vn
+
+
 class ObjParser:
 
   vertexes = list()
+  vertexes_textures = list()
   faces = list()
 
 
   def __init__(self, file_path) -> None:
     file = open(file_path)
     self._parse_vertexes(file)
+    self._parse_vertexes_textures(file)
     self._parse_faces(file)
     
     
@@ -18,11 +31,22 @@ class ObjParser:
   def _parse_vertexes(self, file):
     while((line := file.readline())[0] == "v"):
       line = line[1:]
-#      vertex = list(map(float, line.split()))
       vertex = list()
       for str_coord in line.split():
         vertex.append(float(str_coord))
       self.vertexes.append(Vec3(vertex[0], vertex[1], vertex[2]))
+
+
+  def _parse_vertexes_textures(self, file):
+    while ((line := file.readline())[0:2] != "vn"):
+
+      if(line[0:2] == "vt"):
+        line = line[2:]
+        vt = list()
+        for str_coord in line.split():
+          vt.append(float(str_coord))
+        self.vertexes_textures.append(Vec3(vt[0], vt[1], 0))
+
 
   def _parse_faces(self, file):
     while ((line := file.readline()) != ""):
@@ -31,4 +55,6 @@ class ObjParser:
         line = line[1:]
         self.faces.append(list())
         for s in line.split():
-          self.faces[-1].append(int(s.split("/")[0]))
+          face = s.split("/")[0:2]
+          face = Face(int(face[0]), int(face[1]))
+          self.faces[-1].append(face)
